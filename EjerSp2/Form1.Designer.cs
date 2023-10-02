ffb53e9fd690931b8f193dd1364fda068dd94a58
FedericoG2 <fg0170700@gmail.com>
2023-10-02 19:17:31 -0300

    Agregar archivos de proyecto.

---
+++ EjerSp2/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace EjerSp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtImporte = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtCantidad = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtPorcentaje = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtDescuento = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtTotal = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Importe";
+            // 
+            // txtImporte
+            // 
+            this.txtImporte.Location = new System.Drawing.Point(141, 21);
+            this.txtImporte.Name = "txtImporte";
+            this.txtImporte.Size = new System.Drawing.Size(71, 20);
+            this.txtImporte.TabIndex = 1;
+            this.txtImporte.TextChanged += new System.EventHandler(this.txtImporte_TextChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(19, 65);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(109, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Cantidad de Artículos";
+            // 
+            // txtCantidad
+            // 
+            this.txtCantidad.Location = new System.Drawing.Point(141, 62);
+            this.txtCantidad.Name = "txtCantidad";
+            this.txtCantidad.Size = new System.Drawing.Size(71, 20);
+            this.txtCantidad.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(30, 128);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "% de Descuento";
+            // 
+            // txtPorcentaje
+            // 
+            this.txtPorcentaje.Location = new System.Drawing.Point(153, 125);
+            this.txtPorcentaje.Name = "txtPorcentaje";
+            this.txtPorcentaje.ReadOnly = true;
+            this.txtPorcentaje.Size = new System.Drawing.Size(42, 20);
+            this.txtPorcentaje.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(31, 168);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(59, 13);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Descuento";
+            // 
+            // txtDescuento
+            // 
+            this.txtDescuento.Location = new System.Drawing.Point(153, 165);
+            this.txtDescuento.Name = "txtDescuento";
+            this.txtDescuento.ReadOnly = true;
+            this.txtDescuento.Size = new System.Drawing.Size(71, 20);
+            this.txtDescuento.TabIndex = 7;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(30, 211);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(31, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Total";
+            // 
+            // txtTotal
+            // 
+            this.txtTotal.Location = new System.Drawing.Point(153, 208);
+            this.txtTotal.Name = "txtTotal";
+            this.txtTotal.ReadOnly = true;
+            this.txtTotal.Size = new System.Drawing.Size(71, 20);
+            this.txtTotal.TabIndex = 9;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(284, 29);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(86, 37);
+            this.btnCalcular.TabIndex = 10;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.txtImporte);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.txtCantidad);
+            this.groupBox1.Location = new System.Drawing.Point(12, 10);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(231, 99);
+            this.groupBox1.TabIndex = 11;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Compra";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(403, 271);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtTotal);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtDescuento);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.txtPorcentaje);
+            this.Controls.Add(this.label3);
+            this.Name = "Form1";
+            this.Text = "Compras";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtImporte;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtCantidad;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtPorcentaje;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtDescuento;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtTotal;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.GroupBox groupBox1;
+    }
+}
+
---
+++ EjerSp2/Form1.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+//Se ingresa un importe (tipo float)
+//Se ingresa la cantidad de artículos (tipo int o float)
+//Se calcula el descuento:
+//-Si el importe es menor o igual a 10000 No hay descuento
+//-Si el importe es mayor a 10000 y la cantidad de artículos es menor o igual a 5 el descuento es del 10%
+//-Si el importe es mayor a 10000 y la cantidad de artículos es mayor a 5 y menor a 10 el descuento es del 15%
+//-Si el importe es mayor a 50000 y la cantidad de artículos es mayor o igual a 10 el descuento es del 20%
+
+//Con esas condiciones se obtiene el % de descuento y el valor del descuento
+//Finalmente se calcula el Total:
+//Total = Importe - Descuento
+//Mostrar los 3 valores calculados
+
+namespace EjerSp1
+{
+    public partial class Form1 : Form
+    {
+        float importe;
+        int cantidadArticulos;
+        float descuento;
+        string porcentaje;
+        float total;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //Valor de importe que pone el usuario
+            
+            importe = float.Parse(txtImporte.Text); //Los valores de una caja son de tipo string entonces lo paso a float
+
+            //Valor de articulos que pone el usuario
+            cantidadArticulos = int.Parse(txtCantidad.Text);
+
+            if (importe <= 10000)
+            {
+                descuento = 0; //Cantidad de descuento
+                porcentaje = "0%"; // Porcentaje de descuento para la etiqueta
+                total = importe;
+
+            }
+
+            if (importe > 50000 && cantidadArticulos >= 10) 
+            {
+                descuento = importe * 0.20f; //Cantidad de descuento
+                porcentaje = "20%"; // Porcentaje de descuento para la etiqueta
+                total = importe - descuento;
+               
+            }
+
+            if (importe > 10000 && cantidadArticulos <= 5)
+            {
+                descuento = importe * 0.1f; //Cantidad de descuento
+                porcentaje = "10%"; // Porcentaje de descuento para la etiqueta
+                total = importe - descuento;
+
+            }
+            if (importe > 10000 && (cantidadArticulos > 5 && cantidadArticulos < 10)) 
+            {
+                descuento = importe *  0.15f; //Cantidad de descuento
+                porcentaje = "15%"; // Porcentaje de descuento para la etiqueta
+                total = importe - descuento;
+
+            }
+
+
+            // Etiqueta con el valor de porcentaje
+            txtPorcentaje.Text = porcentaje;
+
+            //Etiqueta con el valor de descuento 
+            txtDescuento.Text = descuento.ToString();
+
+            //Etiqueta con el valor de total 
+            txtTotal.Text = total.ToString();
+
+
+        }
+
+        private void txtImporte_TextChanged(object sender, EventArgs e)
+        {
+           if(txtImporte.Text == "")
+            {
+                btnCalcular.Enabled = false;
+            } else
+            {
+                btnCalcular.Enabled= true;
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            btnCalcular.Enabled = false;
+        }
+    }
+}
